d27544e0d0f221aea19af4940bc05775f7b2598e
pb <96528023+sfarfsge@users.noreply.github.com>
2023-01-18 03:37:49 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,5 @@
 sed -i "s#UUID#$UUID#g;s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/v2ray/config.json
 sed -i "s#VMESS_WSPATH#$VMESS_WSPATH#g;s#VLESS_WSPATH#$VLESS_WSPATH#g" /etc/nginx/nginx.conf
 exec "$@"
+# 将这行代码中的域名替换成 Replit 为你项目分配的域名
+while true ;do curl https://xx.xx.glitch.me; sleep 300; done;
